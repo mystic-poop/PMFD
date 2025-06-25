2f08906295ae7b12942edcc92a1585c61f3936f2
Forkdev <enesyildiz320@icloud.com>
2025-06-25 10:14:22 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Update the Apt to prevent future apt errors 
+sudo apt update
+
+# Install package managers
+sudo apt install -y snapd aptitude flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+# Asking permission for plugins
+read -p "Do you want to install KDE plugins for flatpak? [Y/n] " kdechoice
+
+if [[ "$kdechoice" =~ [Yy] || -z "$kdechoice" ]]; then
+    sudo apt install -y plasma-discover-backend-flatpak
+else
+    read -p "Would you like to install GNOME addon for flatpak? [Y/n] " gnomechoice
+    
+    if [[ "$gnomechoice" =~ [Yy] || -z "$gnomechoice" ]]; then
+        sudo apt install -y gnome-software-plugin-flatpak
+    else
+        echo "Setup completed successfully. Please restart your computer for better experience."
+    fi
+fi
